1fd2076f6e52ab5098bb151f0096705da2e7ea88
lizcraig <84337943+lizcraig@users.noreply.github.com>
2021-10-25 10:09:30 -0400

    gamelist

---
+++ app/public/js/games.js
@@ -0,0 +1,117 @@
+const GameApp = {
+    data() {
+      return {
+        games: [],
+        // selectedBook: null, //maybe
+        gameForm: {},
+        selectedGame: null
+      }
+    },
+    computed: {},
+    methods: {
+        prettyDollar(n) {
+            const d = new Intl.NumberFormat("en-US").format(n);
+            return "$ " + d;
+        },
+        fetchGameData() {
+            fetch('/api/games/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.games = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        postGame(evt) {
+            if (this.selectedGame === null) {
+                this.postNewGame(evt);
+            } else {
+                this.postEditGame(evt);
+            }
+          },
+        postNewGame(evt) {
+            //this.bookForm.id = this.selectedBook.id; //not sure about this
+            console.log("Posted!", this.gameForm); //test to make sure form works  //use cmd+shift+r to make sure
+            alert("Post");
+
+            fetch('api/games/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.gameForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.games = json; //idk
+                
+                // reset the form
+                this.gameForm = {};
+              });
+
+            
+            },
+            postEditGame(evt) {
+                // this.offerForm.studentId = this.selectedStudent.id;
+                this.gameForm.id = this.games.id;       
+                
+                console.log("Updating!", this.gameForm);
+        
+                fetch('api/games/update.php', {
+                    method:'POST',
+                    body: JSON.stringify(this.gameForm),
+                    headers: {
+                      "Content-Type": "application/json; charset=utf-8"
+                    }
+                  })
+                  .then( response => response.json() )
+                  .then( json => {
+                    console.log("Returned from post:", json);
+                    // TODO: test a result was returned!
+                    this.games = json;
+                    
+                    this.resetGameForm();
+                  });
+              },
+              postDeleteGame(o) {
+                if (!confirm("Are you sure you want to delete the game from "+o.field+"?")) {
+                    return;
+                }
+                
+                fetch('api/games/delete.php', {
+                    method:'POST',
+                    body: JSON.stringify(o),
+                    headers: {
+                      "Content-Type": "application/json; charset=utf-8"
+                    }
+                  })
+                  .then( response => response.json() )
+                  .then( json => {
+                    console.log("Returned from post:", json);
+                    // TODO: test a result was returned!
+                    this.games = json;
+                    
+                    this.resetGameForm();
+                  });
+              },
+              selectGame(o) {
+                this.selectedGame = o;
+                this.gameForm = Object.assign({}, this.selectedGame);
+              },
+              resetGameForm() {
+                this.selectedGame = null;
+                this.gameForm = {};
+              }
+        
+    },
+    created() {
+            this.fetchGameData();
+        }
+  
+  }
+  
+  Vue.createApp(GameApp).mount('#gameApp');
