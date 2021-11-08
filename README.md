dbfd71bd0a07e61b454438cd66dfaef538dbadb4
jeruszko <jeruszko@iu.edu>
2021-11-08 00:31:09 -0500

    merge fail

--- README.md
+++
@@ -1,6 +0,0 @@
-# team18
-MSIS
-jeruszko
-lizcraig2
-salverma08
-willaust
--- app/public/js/admin.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-
-    const uname = sessionStorage.getItem('NAME');
-    
-    document.getElementById('aname').innerHTML = uname;
-    
-
-})
